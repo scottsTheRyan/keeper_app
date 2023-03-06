f8139dc2b4dedd8d183133f0bd4319085db5e334
thedarkness <thedarkness@thedarknesss-MacBook-Pro.local>
2023-03-05 19:24:32 -0800

    Implementing map functions, refactor w arrow functions)

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Header() {
-    return <header>
-        <h1>Keeper Keeper Keeper!!! (Notetaking app)</h1>
-    </header>
-}
-
-export default Header;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from "./components/App.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import React from "react";
+import { createRoot } from 'react-dom/client'
+import App from "./components/App";
+ 
+createRoot(document.getElementById('root')).render(<App />)
